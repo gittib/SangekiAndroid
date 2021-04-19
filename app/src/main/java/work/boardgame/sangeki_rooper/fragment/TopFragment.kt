3e2732447ab99f32ff1a67ad558e3542f31d5e66
gitti <gitti.akihisa.akihisa@gmail.com>
2021-04-19 20:59:55 +0900

    クリエイティブコモンズ表示

--- app/src/main/java/work/boardgame/sangeki_rooper/fragment/TopFragment.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/fragment/TopFragment.kt
@@ -9,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.top_fragment.view.*
 import work.boardgame.sangeki_rooper.R
 import work.boardgame.sangeki_rooper.fragment.viewmodel.TopViewModel
+import work.boardgame.sangeki_rooper.util.Define
 import work.boardgame.sangeki_rooper.util.Logger
 
 class TopFragment : BaseFragment() {
@@ -31,6 +32,10 @@ class TopFragment : BaseFragment() {
             }
             rv.scenario_list_image.setOnClickListener { rv.scenario_list_title.performClick() }
             rv.scenario_list_note.setOnClickListener { rv.scenario_list_title.performClick() }
+
+            rv.footer_text.text = rv.footer_text.text.toString()
+                    .replace("__SANGEKI_ROOPER_URL__", Define.SangekiRooperUrl.TOP)
+                    .replace("__CREATIVE_COMMONS_URL__", Define.SangekiRooperUrl.CREATIVE_COMMONS)
 
             rv
         }
--- app/src/main/java/work/boardgame/sangeki_rooper/util/Define.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/util/Define.kt
@@ -1,6 +1,11 @@
 package work.boardgame.sangeki_rooper.util
 
 object Define {
+    object SangekiRooperUrl {
+        const val TOP = "http://bakafire.main.jp/rooper/sr_top.htm"
+        const val CREATIVE_COMMONS = "http://creativecommons.org/licenses/by-sa/2.1/jp/"
+    }
+
     object SharedPreferencesKey {
         const val SCENARIOS = "SCENARIOS"
         const val USER_AGENT = "USER_AGENT"
