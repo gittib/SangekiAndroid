74a8f7008ebd0c358907ea540cb965d908d95c0f
gitti <gitti.akihisa.akihisa@gmail.com>
2021-10-07 08:44:54 +0900

    脚本リスト更新を手動にする

--- app/src/main/java/work/boardgame/sangeki_rooper/fragment/BaseFragment.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/fragment/BaseFragment.kt
@@ -3,17 +3,8 @@ package work.boardgame.sangeki_rooper.fragment
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.fragment.app.Fragment
-import com.google.gson.Gson
-import io.reactivex.SingleObserver
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import work.boardgame.sangeki_rooper.activity.ContainerActivity
-import work.boardgame.sangeki_rooper.model.TragedyScenarioModel
-import work.boardgame.sangeki_rooper.util.Define
 import work.boardgame.sangeki_rooper.util.Logger
-import work.boardgame.sangeki_rooper.util.Util
-import java.util.*
 
 abstract class BaseFragment: Fragment() {
     private val TAG = BaseFragment::class.simpleName
@@ -22,38 +13,8 @@ abstract class BaseFragment: Fragment() {
     protected lateinit var activity:ContainerActivity
 
     override fun onAttach(context: Context) {
+        Logger.v(TAG, "■--- onAttach -----")
         super.onAttach(context)
         activity = context as ContainerActivity
-        dailyUpdate(context)
-    }
-
-    /** 日次更新処理 */
-    private fun dailyUpdate(context: Context) {
-        val now = Calendar.getInstance().timeInMillis
-        val lastUpdated = prefs.getLong(Define.SharedPreferencesKey.LAST_UPDATED_SCENARIO, 0L)
-        val oneDay = 24 * 3600 * 1000L
-        if (now - lastUpdated >= oneDay) {
-            Logger.i(TAG, "前回のデータ更新から1日以上時間経過してるので、データ更新処理を実行する")
-            Util.getRxRestInterface(context)
-                .getScenarioList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object: SingleObserver<List<TragedyScenarioModel>> {
-                    override fun onSuccess(t: List<TragedyScenarioModel>) {
-                        Logger.i(TAG, "脚本データ更新")
-                        prefs.edit()
-                                .putString(Define.SharedPreferencesKey.SCENARIOS, Gson().toJson(t))
-                                .putLong(Define.SharedPreferencesKey.LAST_UPDATED_SCENARIO, now)
-                                .apply()
-                    }
-
-                    override fun onSubscribe(d: Disposable) {}
-                    override fun onError(e: Throwable) {
-                        Logger.w(TAG, Throwable(e))
-                    }
-                })
-        } else {
-            Logger.v(TAG, "まだ1日経過してないのでデータ更新は実行しない")
-        }
     }
 }
--- app/src/main/java/work/boardgame/sangeki_rooper/fragment/ScenarioListFragment.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/fragment/ScenarioListFragment.kt
@@ -10,15 +10,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import io.reactivex.SingleObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.adapter_item_scenario.view.*
 import kotlinx.android.synthetic.main.scenario_list_fragment.view.*
 import work.boardgame.sangeki_rooper.R
 import work.boardgame.sangeki_rooper.fragment.viewmodel.ScenarioListViewModel
+import work.boardgame.sangeki_rooper.model.TragedyScenarioModel
+import work.boardgame.sangeki_rooper.util.Define
 import work.boardgame.sangeki_rooper.util.Logger
 import work.boardgame.sangeki_rooper.util.Util
+import java.util.*
+import kotlin.Comparator
 
 class ScenarioListFragment : BaseFragment() {
     private val TAG = ScenarioListFragment::class.simpleName
@@ -40,26 +50,70 @@ class ScenarioListFragment : BaseFragment() {
                 it.layoutManager = LinearLayoutManager(activity)
                 it.adapter = ScenarioListAdapter()
             }
-            rv.show_scenario_title.setOnClickListener {
-                if (viewModel.showTitle) {
-                    AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
-                            .setMessage("脚本タイトルを非表示にしますか？")
-                            .setPositiveButton(R.string.ok) { _, _ ->
-                                viewModel.showTitle = false
-                                rv.scenario_list.adapter?.notifyDataSetChanged()
-                            }
-                            .setNegativeButton(R.string.cancel, null)
-                            .show()
-                } else {
-                    AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
-                            .setMessage("脚本タイトルを表示してもよろしいですか？\n（※ネタバレになる可能性があります）")
-                            .setPositiveButton(R.string.ok) { _, _ ->
-                                viewModel.showTitle = true
-                                rv.scenario_list.adapter?.notifyDataSetChanged()
-                            }
-                            .setNegativeButton(R.string.cancel, null)
-                            .show()
+            rv.show_scenario_nav.setOnClickListener {
+                rv.scenario_list_layout.let { v ->
+                    if (v.isDrawerOpen(GravityCompat.END)) v.closeDrawer(GravityCompat.END)
+                    else v.openDrawer(GravityCompat.END)
                 }
+            }
+            rv.scenario_list_nav.setNavigationItemSelectedListener {item ->
+                when (item.itemId) {
+                    R.id.show_title -> {
+                        if (viewModel.showTitle) {
+                            AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
+                                    .setMessage("脚本タイトルを非表示にしますか？")
+                                    .setPositiveButton(R.string.ok) { _, _ ->
+                                        viewModel.showTitle = false
+                                        rv.scenario_list.adapter?.notifyDataSetChanged()
+                                    }
+                                    .setNegativeButton(R.string.cancel, null)
+                                    .show()
+                        } else {
+                            AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
+                                    .setMessage("脚本タイトルを表示してもよろしいですか？\n（※ネタバレになる可能性があります）")
+                                    .setPositiveButton(R.string.ok) { _, _ ->
+                                        viewModel.showTitle = true
+                                        rv.scenario_list.adapter?.notifyDataSetChanged()
+                                    }
+                                    .setNegativeButton(R.string.cancel, null)
+                                    .show()
+                        }
+                    }
+                    R.id.update_list -> {
+                        Util.getRxRestInterface(activity)
+                                .getScenarioList()
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(object: SingleObserver<List<TragedyScenarioModel>> {
+                                    override fun onSuccess(t: List<TragedyScenarioModel>) {
+                                        prefs.edit()
+                                                .putString(Define.SharedPreferencesKey.SCENARIOS, Gson().toJson(t))
+                                                .putLong(Define.SharedPreferencesKey.LAST_UPDATED_SCENARIO, Calendar.getInstance().timeInMillis)
+                                                .apply()
+
+                                        AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
+                                                .setMessage("脚本リストを最新化しました。")
+                                                .setOnDismissListener {
+                                                    rv.scenario_list.adapter?.notifyDataSetChanged()
+                                                }
+                                                .show()
+                                    }
+
+                                    override fun onSubscribe(d: Disposable) {
+                                    }
+
+                                    override fun onError(e: Throwable) {
+                                        Logger.w(TAG, Throwable(e))
+                                        AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
+                                                .setMessage("脚本リストの最新化に失敗しました。\n少し時間をあけて、再度お試しください。")
+                                                .show()
+                                    }
+                                })
+                    }
+                }
+                rv.scenario_list_layout.closeDrawer(GravityCompat.END)
+
+                true
             }
         }
 
