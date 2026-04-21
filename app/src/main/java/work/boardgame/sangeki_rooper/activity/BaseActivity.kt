06b4fca1ebe321b25e30607ef7f517a65f5c6c13
gitti <gitti.akihisa.akihisa@gmail.com>
2026-04-21 18:04:35 +0900

    通知アイコン表示制御

--- app/src/main/java/work/boardgame/sangeki_rooper/activity/BaseActivity.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/activity/BaseActivity.kt
@@ -3,9 +3,11 @@ package work.boardgame.sangeki_rooper.activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.ActivityInfo
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
 import work.boardgame.sangeki_rooper.R
 import work.boardgame.sangeki_rooper.util.Define
 import work.boardgame.sangeki_rooper.util.Logger
@@ -16,6 +18,13 @@ abstract class BaseActivity:AppCompatActivity() {
     val prefs:SharedPreferences by lazy {
         getSharedPreferences(Define.SHARED_PREFERENCES_NAME, Context.MODE_PRIVATE) }
 
+    // ダークモードかどうかを判定するプロパティ
+    val isDarkMode: Boolean
+        get() {
+            val nightModeFlags = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+            return nightModeFlags == Configuration.UI_MODE_NIGHT_YES
+        }
+
     private var progressCount = 0
     private var progressBar:View? = null
 
@@ -23,6 +32,8 @@ abstract class BaseActivity:AppCompatActivity() {
         Logger.methodStart(TAG)
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        WindowInsetsControllerCompat(window, window.decorView).isAppearanceLightStatusBars = !isDarkMode
     }
 
     override fun onStart() {
