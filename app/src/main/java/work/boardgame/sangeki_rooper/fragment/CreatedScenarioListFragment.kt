657a83391ce74f8a252368a6a0322dee407a1c6b
gitti <gitti.akihisa.akihisa@gmail.com>
2026-07-18 15:05:07 +0900

    リスト表示できた

--- app/src/main/java/work/boardgame/sangeki_rooper/fragment/CreatedScenarioListFragment.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/fragment/CreatedScenarioListFragment.kt
@@ -1,15 +1,20 @@
 package work.boardgame.sangeki_rooper.fragment
 
+import android.app.AlertDialog
 import android.content.Context
+import android.graphics.BlendMode
+import android.graphics.BlendModeColorFilter
+import android.graphics.PorterDuff
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.gson.Gson
 import io.reactivex.SingleObserver
 import io.reactivex.disposables.Disposable
@@ -23,6 +28,9 @@ import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeoutOrNull
 import retrofit2.HttpException
 import work.boardgame.sangeki_rooper.R
+import work.boardgame.sangeki_rooper.databinding.AdapterItemFooterBinding
+import work.boardgame.sangeki_rooper.databinding.AdapterItemScenarioBinding
+import work.boardgame.sangeki_rooper.databinding.AdapterItemScenarioHeaderBinding
 import work.boardgame.sangeki_rooper.databinding.CreatedScenarioListFragmentBinding
 import work.boardgame.sangeki_rooper.fragment.viewmodel.CreatedScenarioListViewModel
 import work.boardgame.sangeki_rooper.model.CreatedScenarioCacheModel
@@ -71,13 +79,17 @@ class CreatedScenarioListFragment : BaseFragment() {
         Logger.methodStart(TAG)
         binding = CreatedScenarioListFragmentBinding.inflate(inflater, container, false).also { rv ->
 
+            rv.scenarioList.let {
+                it.layoutManager = LinearLayoutManager(activity)
+                it.adapter = ScenarioListAdapter()
+            }
+
             rv.createdScenarioListNav.setNavigationItemSelectedListener { item ->
                 when (item.itemId) {
                     R.id.show_title -> {
                         TODO("脚本タイトルの表示非表示切り替え")
                     }
                     R.id.update_list -> {
-                        rv.createdScenarioListLayout.closeDrawer(GravityCompat.END)
                         AlertDialog.Builder(activity, R.style.Theme_SangekiAndroid_DialogBase)
                             .setMessage("脚本リストを最新の状態に更新します。よろしいですか？")
                             .setPositiveButton(android.R.string.ok) { _, _ ->
@@ -85,6 +97,7 @@ class CreatedScenarioListFragment : BaseFragment() {
                             }
                             .setNegativeButton(android.R.string.cancel, null)
                             .show()
+                        rv.createdScenarioListLayout.closeDrawer(GravityCompat.END)
                     }
                     R.id.go_to_old_list -> {
                         activity.startFragment(ScenarioListFragment::class.qualifiedName)
@@ -117,8 +130,9 @@ class CreatedScenarioListFragment : BaseFragment() {
                     val cachedAt = loadFromCache(context)?.let {
                         viewModel.scenarioList.clear()
                         viewModel.scenarioList.addAll(it.scenarios)
-                        binding?.scenarioList?.adapter?.notifyDataSetChanged()
-                        dismissProgress()
+                        viewModel.viewModelScope.launch {
+                            binding?.scenarioList?.adapter?.notifyDataSetChanged()
+                        }
                         it.cachedAt
                     } ?: -1L
 
@@ -260,4 +274,85 @@ class CreatedScenarioListFragment : BaseFragment() {
             }
         }
     }
+
+    private object ViewType {
+        const val HEADER = 0
+        const val SCENARIO = 1
+        const val FOOTER = 99
+    }
+    private inner class ScenarioListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+        inner class ScenarioViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+            fun onBind(position: Int) {
+                val item = viewModel.scenarioList[position-1]
+                AdapterItemScenarioBinding.bind(itemView).let { rv ->
+                    rv.scenarioId.text = String.format("[%s]", item.id)
+                    rv.recommendedScenario.visibility = when (item.recommended) {
+                        true -> View.VISIBLE
+                        else -> View.GONE
+                    }
+                    rv.tragedySet.let { v ->
+                        v.text = item.set
+                        val d = v.background
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                            d?.colorFilter = BlendModeColorFilter(item.tragedySetColor(), BlendMode.SRC_IN)
+                        } else {
+                            d?.setTint(item.tragedySetColor())
+                            d?.setTintMode(PorterDuff.Mode.SRC_IN)
+                        }
+                    }
+                    rv.scenarioTitle.let { v ->
+                        v.text = item.title
+                        v.visibility = when (viewModel.showTitle) {
+                            true -> View.VISIBLE
+                            else -> View.GONE
+                        }
+                    }
+                    rv.difficultyName.text = item.difficultyName()
+                    rv.difficulty.text = item.difficultyStar()
+                    rv.loop.text = item.loop()
+                    rv.day.text = item.day.toString()
+                    rv.scenarioTitle.text = item.title
+                    rv.writer.text = String.format(getString(R.string.writer_introduction), item.writer)
+
+                    rv.root.setOnClickListener {
+                        activity.startFragment(ScenarioDetailFragment::class.qualifiedName, item.id)
+                    }
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val inflater = LayoutInflater.from(activity)
+            return when (viewType) {
+                ViewType.HEADER -> {
+                    val v = AdapterItemScenarioHeaderBinding.inflate(inflater, parent, false)
+                    object: RecyclerView.ViewHolder(v.root){}
+                }
+                ViewType.SCENARIO -> {
+                    val v = AdapterItemScenarioBinding.inflate(inflater, parent, false)
+                    ScenarioViewHolder(v.root)
+                }
+                ViewType.FOOTER -> {
+                    val v = AdapterItemFooterBinding.inflate(inflater, parent, false)
+                    object: RecyclerView.ViewHolder(v.root){}
+                }
+                else -> throw IllegalArgumentException("invalid view type: $viewType")
+            }
+        }
+
+        override fun getItemCount(): Int = viewModel.scenarioList.size + 2
+
+        override fun getItemViewType(position: Int): Int = when (position) {
+            0 -> ViewType.HEADER
+            itemCount-1 -> ViewType.FOOTER
+            else -> ViewType.SCENARIO
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            when (getItemViewType(position)) {
+                ViewType.SCENARIO -> (holder as ScenarioViewHolder).onBind(position)
+            }
+        }
+    }
+
 }
--- app/src/main/java/work/boardgame/sangeki_rooper/fragment/viewmodel/CreatedScenarioListViewModel.kt
+++ app/src/main/java/work/boardgame/sangeki_rooper/fragment/viewmodel/CreatedScenarioListViewModel.kt
@@ -5,6 +5,7 @@ import work.boardgame.sangeki_rooper.model.TragedyScenarioModel
 import java.util.concurrent.CopyOnWriteArrayList
 
 class CreatedScenarioListViewModel : ViewModel() {
+    var showTitle: Boolean = false
     val scenarioList = CopyOnWriteArrayList<TragedyScenarioModel>()
     var progressCount: Int = 0
 }
