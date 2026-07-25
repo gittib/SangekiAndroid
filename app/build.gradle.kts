ae4bbc8c6a6086e365a1a2c2901f70d63ccd7264
gitti <gitti.akihisa.akihisa@gmail.com>
2026-07-25 16:39:56 +0900

    アプリバージョン更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "work.boardgame.sangeki_rooper"
         minSdk = 24
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.6.1"
+        versionCode = 33
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
